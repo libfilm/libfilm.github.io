6119cc82e44af965787cdbbe2e6f9fc1964e6a75
libfilm <66459866+libfilm@users.noreply.github.com>
2020-06-05 10:00:21 +0300

    first commit

---
+++ precache-manifest.fc4e44bca9d218c4f5611939c4d22721.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "697fa7d4827207435607dec46fd610b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f2ceec26223826e06c6",
+    "url": "/static/css/2.87a4a1bf.chunk.css"
+  },
+  {
+    "revision": "a990a2adb3c4aaabffd0",
+    "url": "/static/css/main.dc88b2f9.chunk.css"
+  },
+  {
+    "revision": "4f2ceec26223826e06c6",
+    "url": "/static/js/2.918751d7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.918751d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a990a2adb3c4aaabffd0",
+    "url": "/static/js/main.f1c7303c.chunk.js"
+  },
+  {
+    "revision": "dd1be62d932e68ef6b31",
+    "url": "/static/js/runtime-main.5f5524ed.js"
+  }
+]);
