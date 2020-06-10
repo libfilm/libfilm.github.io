64912032e5b7dcd789ac06035e643fca291bf4ea
libfilm <njg_dm@rambler.ru>
2020-06-10 21:23:58 +0300

    Updates

---
+++ precache-manifest.d5db59eb550408c80d8bc07a30736e1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b9016f353ca5e8d32ba6077610bd39d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3201401127ab484a3f14",
+    "url": "/static/css/2.87a4a1bf.chunk.css"
+  },
+  {
+    "revision": "8a17dc2540206380283e",
+    "url": "/static/css/main.480b23b6.chunk.css"
+  },
+  {
+    "revision": "3201401127ab484a3f14",
+    "url": "/static/js/2.96d5b782.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.96d5b782.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a17dc2540206380283e",
+    "url": "/static/js/main.0cc7bdb9.chunk.js"
+  },
+  {
+    "revision": "dd1be62d932e68ef6b31",
+    "url": "/static/js/runtime-main.5f5524ed.js"
+  }
+]);
